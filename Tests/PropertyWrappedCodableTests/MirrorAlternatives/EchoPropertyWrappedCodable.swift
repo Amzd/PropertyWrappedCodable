12e7a561cf66b659292643bdba30b246707f8966
Casper Zandbergen <casper@yaacomm.com>
2019-12-13 11:37:29 +0100

    Echo support for quick version switching

--- Tests/PropertyWrappedCodableTests/MirrorAlternatives/EchoPropertyWrappedCodable.swift
+++ Tests/PropertyWrappedCodableTests/MirrorAlternatives/EchoPropertyWrappedCodable.swift
@@ -1,34 +1,42 @@
-@testable import Echo
-@testable import PropertyWrappedCodable
+#if canImport(EchoMirror) || canImport(Echo)
+    #if canImport(EchoMirror)
+        @testable import EchoMirror
+    #elseif canImport(Echo)
+        @testable import Echo
+        typealias EchoMirror = Echo.Mirror
+    #endif
 
-public protocol EchoPropertyWrappedCodable: Codable {
-    /// Warning: Property wrapped values arent available here yet!
-    init(nonWrappedValuesFrom decoder: Decoder) throws
-}
+    @testable import PropertyWrappedCodable
 
-public extension EchoPropertyWrappedCodable {
-    init(from decoder: Decoder) throws {
-        try self.init(propertyWrappedValuesFrom: decoder)
+    public protocol EchoPropertyWrappedCodable: Codable {
+        /// Warning: Property wrapped values arent available here yet!
+        init(nonWrappedPropertiesFrom decoder: Decoder) throws
     }
-    
-    /// Call this when you override `init(from:)`
-    init(propertyWrappedValuesFrom decoder: Decoder) throws {
-        try self.init(nonWrappedValuesFrom: decoder)
-        let container = try decoder.container(keyedBy: AnyCodingKey.self)
+
+    public extension EchoPropertyWrappedCodable {
+        init(from decoder: Decoder) throws {
+            try self.init(wrappedPropertiesFrom: decoder)
+        }
         
-        let mirror = Mirror(reflecting: self)
-        for child in mirror.children {
-            let value = child.value as? CodableValueProtocol
-            try value?.initValue(with: child.label!, from: container)
+        /// Call this when you override `init(from:)`
+        init(wrappedPropertiesFrom decoder: Decoder) throws {
+            try self.init(nonWrappedPropertiesFrom: decoder)
+            let container = try decoder.container(keyedBy: AnyCodingKey.self)
+            
+            let mirror = EchoMirror(reflecting: self)
+            for child in mirror.children {
+                let value = child.value as? CodableValueProtocol
+                try value?.initValue(with: child.label!, from: container)
+            }
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            try encode(propertyWrappedValuesTo: encoder)
+        }
+        
+        /// Call this when you override `encode(to:)`
+        func encode(propertyWrappedValuesTo encoder: Encoder) throws {
+            fatalError()
         }
     }
-    
-    func encode(to encoder: Encoder) throws {
-        try encode(propertyWrappedValuesTo: encoder)
-    }
-    
-    /// Call this when you override `encode(to:)`
-    func encode(propertyWrappedValuesTo encoder: Encoder) throws {
-        fatalError()
-    }
-}
+#endif
