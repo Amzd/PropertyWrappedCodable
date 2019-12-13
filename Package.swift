a502ceb91a776c233cd17e6f3efeaa9b164d955a
Casper Zandbergen <casper@yaacomm.com>
2019-12-13 11:39:57 +0100

    Added Echo to package for quick switching to current master

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/wickwirew/Runtime", from: "2.1.1"),
+        // before breaking issues:
         .package(url: "https://github.com/Azoy/Echo", .revision("92d09d75e382cb7e4fd0ed02bca445d0d7958207"))
+        // current:
+//        .package(url: "https://github.com/Azoy/Echo", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
